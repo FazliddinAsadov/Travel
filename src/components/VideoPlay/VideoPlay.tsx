daebec736115040d7a6eb70890eb6dd9486a9857
FazliddinAsadov <asadovfazliddin11@gmail.com>
2023-02-23 09:22:48 +0500

    add youtube video

--- src/components/VideoPlay/VideoPlay.tsx
+++ src/components/VideoPlay/VideoPlay.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { Box, Text } from "@mantine/core";
+import React, { useState } from "react";
+import { Box, Modal, Text } from "@mantine/core";
 import useStayles from "./Style/videoPlayStyle";
 import videoPlayLocaleEn from "./locale";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 import img from "@assets/Namangan1.jpg";
+import YoutubeVideo from "./components/YoutubeVideo";
 
 const VideoPlay = () => {
   const { classes } = useStayles();
+  const [opened, setOpened] = useState(false);
+
   return (
     <Box className={classes.bg}>
       <Box className={classes.head}>
@@ -15,10 +18,21 @@ const VideoPlay = () => {
         <Box className={classes.line}></Box>
         <Box className={classes.iconItem}>
           <Text>{videoPlayLocaleEn.click}</Text>
-          <BsFillPlayCircleFill className={classes.icon} />
+          <BsFillPlayCircleFill
+            className={classes.icon}
+            onClick={() => setOpened(true)}
+          />
           <Text>{videoPlayLocaleEn.watch}</Text>
         </Box>
       </Box>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Beautiful Namangan"
+        size={"70%"}
+      >
+        <YoutubeVideo />
+      </Modal>
     </Box>
   );
 };
