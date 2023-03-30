6c88134feb53dabaf949ef18695735352d2ea121
FazliddinAsadov <asadovfazliddin11@gmail.com>
2023-03-31 00:20:32 +0500

    add car  rentals

---
+++ src/modules/carRentals/components/Header/Header.tsx
@@ -0,0 +1,44 @@
+import {
+  createStyles,
+  Text,
+  Title,
+  TextInput,
+  Button,
+  Image,
+  rem,
+} from "@mantine/core";
+
+import useCarRentHeaderStyle from "./useCarRentHeaderStyle";
+
+const CarHeader = () => {
+  const { classes } = useCarRentHeaderStyle();
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.body}>
+        <Title className={classes.title}>Wait a minute...</Title>
+        <Text fw={500} fz="lg" mb={5}>
+          Subscribe to our newsletter!
+        </Text>
+        <Text fz="sm" c="dimmed">
+          You will never miss important product updates, latest news and
+          community QA sessions. Our newsletter is once a week, every Sunday.
+        </Text>
+
+        <div className={classes.controls}>
+          <TextInput
+            placeholder="Your email"
+            classNames={{ input: classes.input, root: classes.inputWrapper }}
+          />
+          <Button className={classes.control}>Subscribe</Button>
+        </div>
+      </div>
+      <Image
+        // src="https://blackfoxmotors.de/wp-content/uploads/2021/02/SAM_1211-1920x1280.jpg"
+        src="/images/mersheader-removebg-preview.png"
+        className={classes.image}
+        alt="mers"
+      />
+    </div>
+  );
+};
+export default CarHeader;
