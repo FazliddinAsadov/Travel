cc5e2c469b95da9ddd373e5f3e2f30c2ccfd6e86
FazliddinAsadov <asadovfazliddin11@gmail.com>
2023-04-02 13:28:27 +0500

    update

--- src/components/darkMode/DarkLightMode.tsx
+++ src/components/darkMode/DarkLightMode.tsx
@@ -1,20 +1,31 @@
-import { ActionIcon, useMantineColorScheme } from "@mantine/core";
-import { MdDarkMode } from "react-icons/md";
-import { CiDark } from "react-icons/ci";
+import {
+  Group,
+  Switch,
+  useMantineColorScheme,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconMoonStars, IconSun } from "@tabler/icons-react";
 
 const DarkMode = () => {
+  // const dark = colorScheme === "dark";
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const dark = colorScheme === "dark";
-
+  const theme = useMantineTheme();
   return (
-    <ActionIcon
-      variant="outline"
-      color={dark ? "yellow" : "blue"}
-      onClick={() => toggleColorScheme()}
-      title="Toggle color scheme"
-    >
-      {dark ? <CiDark /> : <MdDarkMode />}
-    </ActionIcon>
+    <Group position="center" my={30}>
+      <Switch
+        checked={colorScheme === "dark"}
+        onChange={() => toggleColorScheme()}
+        size="lg"
+        onLabel={<IconSun color={theme.white} size="1.25rem" stroke={1.5} />}
+        offLabel={
+          <IconMoonStars
+            color={theme.colors.gray[6]}
+            size="1.25rem"
+            stroke={1.5}
+          />
+        }
+      />
+    </Group>
   );
 };
 export default DarkMode;
