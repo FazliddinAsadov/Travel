f3433b99e399dc4c86e8eecaf1822687d7754173
isoxon7954 <isoxon9502@gmail.com>
2023-04-04 20:41:29 +0500

    new card added and responsive finished

--- src/modules/carRentals/CarRentals.tsx
+++ src/modules/carRentals/CarRentals.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import carRentalsLocaleUz from "./locale";
 import CarHeader from "./components/Header/Header";
 import TextPart from "@components/Text/TextPart";
+import Service from "./components/Service/Service"
 type Props = {};
 
 const CarRentals = (props: Props) => {
@@ -12,6 +13,7 @@ const CarRentals = (props: Props) => {
         title={carRentalsLocaleUz.servicesTitle}
         titleTwo={carRentalsLocaleUz.servicesTitleTwo}
       />
+      <Service />
       <TextPart
         title={carRentalsLocaleUz.aboutTitle}
         titleTwo={carRentalsLocaleUz.aboutTitleTwo}
